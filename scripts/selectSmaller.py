a3ecfe43b9b0243802f483f8edb609eb68f2d617
Lucas Tramonte <lucas.tramonte@student-cs.fr>
2025-03-03 16:18:36 +0100

    organization

--- scripts/selectSmaller.py
+++ scripts/selectSmaller.py
@@ -10,7 +10,7 @@ NEW_FILE_NAME = "data/1000samples.geojson"
 N_SAMPLES = 1000
 
 ## Read data
-train_df = gpd.read_file(f'{BASE_PATH}/data/train.geojson', index_col=0)
+train_df = gpd.read_file(f'{BASE_PATH}../Assets/data/train.geojson', index_col=0)
 
 ## Sorting
 train_df_aleatorio = train_df.sample(N_SAMPLES, random_state=1)  # Você pode ajustar random_state para obter resultados reproduzíveis
